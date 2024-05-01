0f49ac8fc1ccc6d1a6e07b6b53fde6ea6caa812e
Alex Bogdos <alxbogdos2004@gmail.com>
2024-05-01 15:01:53 +0300

    Create settings_database.dart

---
+++ min_launcher/lib/src/settings/settings_database.dart
@@ -0,0 +1,56 @@
+import 'package:sqflite/sqflite.dart';
+
+class SettingsDatabase {
+  final String _databasePath = "settings.db";
+  late Database _db;
+
+  // Initialize database. Create if it doesn't exist
+  Future<void> open() async {
+    _db = await openDatabase(
+      _databasePath,
+      version: 1,
+      onCreate: _createDb,
+    );
+  }
+
+  // Create Database
+  Future<void> _createDb(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE settings (
+        title TEXT PRIMARY KEY,
+        value TEXT
+      );
+    ''');
+  }
+
+  // Insert a setting into the database
+  Future<void> insert(String title, dynamic value) async {
+    await _db.insert(
+      'settings',
+      {'title': title, 'value': value.toString()},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> update(String title, dynamic value) async {
+    await _db.update(
+      'settings',
+      {'value': value},
+      where: 'title = ?',
+      whereArgs: [title],
+    );
+  }
+
+  // Get the value of a setting
+  Future<T?> getValue<T>(String title) async {
+    List<Map<String, dynamic>> maps = await _db.query(
+      'settings',
+      where: 'title = ?',
+      whereArgs: [title],
+    );
+    if (maps.isNotEmpty) {
+      return maps.first as T;
+    }
+    return null;
+  }
+}
